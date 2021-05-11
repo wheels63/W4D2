05fe71bfbf9590e63ed7d1e165082300d537f405
kakintse <kinkatse@mail.adelphi.edu>
2021-05-11 15:22:58 -0400

    Worked on recursion and creating node tree

--- employee.rb
+++ employee.rb
@@ -5,7 +5,22 @@ class Employee
         @title = title
         @salary = salary
         @boss = boss
+        @boss.add_employee(self) if !boss.nil?
     end
+
+=begin
+creating the tree
+ned manager instance does not have a boss
+darren manager instance set boss -> ned
+shawna/david employee insances set boss -> darren
+
+searching thru the tree
+ned.employees.each
+    darren.employees - > shawna and david
+    set each employee as a var
+dfs
+
+=end
 
     def bonus(multiplier)
         bonus = @salary * multiplier
--- manager.rb
+++ manager.rb
@@ -1,28 +1,66 @@
 require_relative "employee"
 
 class Manager < Employee
+    attr_reader :name, :title, :salary, :boss, :employees
 
-    def initialize
+    def initialize(name, title, salary, boss) #ned, founder, 100000, nil
         super
         @employees = []
     end
 
-    def populate(name, title, salary, boss)
-        employee = Employee.new(name, title, salary, boss)
+    # def hire_employee(name, title, salary) #ned is boss of the employees added under him so we can call self here
+    #     if condition
+    #         Manager.new(name, title, salary, self)
+    #     else
+    #         Employee.new(name, title, salary, self)
+    #     end
+    #     #ned
+    # end
+
+    def add_employee(employee)
         @employees << employee
     end
+    # add_employee(ned)
+    # ned.employ = [darren]
+    # darren.employ = [shawna, david]
 
     def bonus(multiplier)
         total_emp_sal = 0
-        @employees.each do |employee|
-            total_emp_sal += employee.salary
-        end
+        # @employees.each do |employee|
+        #     total_emp_sal += employee.dfs
+        # end
+        employees.summing_salaries
         bonus = total_emp_sal * multiplier
     end
 
+    def summing_salaries
+        return nil if self.length < 1
+        return self[0] if self.length == 1 
+        
+        self[0] + summing_salaries(self[1..-1])
+    end
+
+    # def recursion
+    #     base case return salary if child = nil
+    #     self.employ.each 
+    #     sum += child.recursion
+    # end
+
+    # def dfs
+    #     return self.salary if self.nil?
+    #     self.employees.each do |child|
+    #       search_child = child.dfs
+    #       return search_child if search_child != nil
+    #     end
+    #     return nil
+    # end
 
 end
 
+ned = Manager.new("Ned", "Founder", 1000000, nil)
+darren = Manager.new("Darren", "TA Manager", 70000, ned)
+shawna = Employee.new("Shawna", "TA", 12000, darren)
+david = Employee.new("David", "TA", 10000, darren)
 
 =begin 
 
