c0cd163dc8a1f7d705c10fb6f55eefd31d072ab9
kakintse <kinkatse@mail.adelphi.edu>
2021-05-12 12:16:52 -0400

    Added some code for grow_unblocked_moves_in_dire

--- Chess/modules/slideable.rb
+++ Chess/modules/slideable.rb
@@ -2,8 +2,9 @@ module Slideable
     
     HORIZONTAL_DIRS = [[1,0], [0,1], [-1,0], [0,-1]]
     DIAGONAL_DIRS = [[1,1], [-1,1], [-1,-1], [1,-1]]
-# + moves
+    # + moves
     # moves array that has all possible moves of the piece from the position you are at
+
     def moves(pos)
         dx = pos[0]
         dy = pos[1]
@@ -19,7 +20,7 @@ module Slideable
         DIAGONAL_DIRS
     end
 
-# - moves_dirs (overwritten by subclass)
+    # - moves_dirs (overwritten by subclass)
     # which valid direction for this piece
     # return HORIZONTAL_DIRS
     # (1,0) (0,1) (-1,0) (0,-1)
@@ -32,19 +33,21 @@ module Slideable
         diagonal_dirs
     end 
 
-# - grow_unblocked_moves_in_dire(dx,dy)
+    # - grow_unblocked_moves_in_dire(dx,dy)
     # until reaches edge or piece, color of piece
-        # then shovel each into the moves array
-# =end
-    def grow_unblocked_moves_in_dire(dx,dy)
-        return dx,dy + 1 if moves_dirs == 
+    # then shovel each into the moves array
+
+    def grow_unblocked_moves_in_dire(color, pos)
+        pos[0] = dx
+        pos[1] = dy
+        moves = []
+        until !(0...8).include?(dx) || !(0...8).include?(dy) || position != null_piece
+        end
     end
 
 end
 =begin
 array of moves piece can move to given current configuration of board
-
-
 
 ----------------
 getter methods
