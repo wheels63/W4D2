92bed215f8c4a17793d02aa3aedd185935a7cc64
kakintse <kinkatse@mail.adelphi.edu>
2021-05-12 16:50:14 -0400

    Fixed up move methods in each sliding subclass and refactored a lot of our ideas

--- Chess/modules/slideable.rb
+++ Chess/modules/slideable.rb
@@ -5,11 +5,13 @@ module Slideable
     # + moves
     # moves array that has all possible moves of the piece from the position you are at
 
-    def moves(pos)
-        dx = pos[0]
-        dy = pos[1]
-        possible_moves = [] 
-        possible_moves << grow_unblocked_moves_in_dire(dx,dy)
+    def moves
+        possible_moves = []
+        moves_dirs.each do |dir|
+            x, y = dir
+            possible_moves += grow_unblocked_moves_in_dire(x,y)
+        end
+        possible_moves
     end
     
     def horizontal_dirs
@@ -29,53 +31,76 @@ module Slideable
     # raise implement or not implement
 
     def moves_dirs
-        horizontal_dirs
-        diagonal_dirs
+        raise "Something went wrong"
     end 
+
+    def grow_unblocked_moves_in_dire(dx,dy)
+        arr = []
+        until !self.board.valid_pos?(pos) || self.board[pos] != self.null_piece
+            pos[0] += dx
+            pos[1] += dy
+            arr << pos
+        end
+        pos[0] += dx
+        pos[1] += dy
+        arr << pos if self.board[pos].color != self.color
+        arr
+    end
 
     # - grow_unblocked_moves_in_dire(dx,dy)
     # until reaches edge or piece, color of piece
     # then shovel each into the moves array
 
-    def grow_unblocked_moves_in_dire(color, pos)
-        dx = pos[0]
-        dy = pos[1]
-        horz_moves = []
-        diag_moves = []
+    # def grow_unblocked_moves_in_dire(dx,dy)
+        # can access piece.rb pos because it is inherited
 
-        # until !(0...8).include?(dx) || !(0...8).include?(dy) || Board.grid[pos] != Board.null_piece && !color
-            # - iterate horizontal_dirs, grab direction, add to x and y, individually
-            # - shovel to moves array every time we increment or decrement x and y
-        dup_x = dx.dup #3
-        dup_y = dy.dup #5
-        new_pos = pos.dup
-        horizontal_dirs.each do |dir| #(0,1)
-            until self.board.valid_pos?(new_pos) || self.board[new_pos]
-                # if my color, we stop
-                # if not my color, take that spot (shovel that position to moves)
-                # if position is empty, take the spot
+        # set new array?
+        # until check if change is in board borders, empty, check piece if other color
+        # do change in position using dx dy on pos from piece class
 
-                # start_pos = (3,5)
-                dup_x += dir[0] # 3 += 0 dup_x = 3
-                dup_y += dir[0] # 5 += 1 dup_y = 6
-                new_pos = [dup_x, dup_y] # (3,6)
+        # dx = pos[0]
+        # dy = pos[1]
+        # horz_moves = []
+        # diag_moves = []
+
+        # dup_x = dx.dup
+        # dup_y = dy.dup
+        # new_pos = pos.dup
+        # horizontal_dirs.each do |dir|
+        #     until !self.board.valid_pos?(new_pos) || self.board[new_pos] != self.null_piece
+        #         dup_x += dir[0]
+        #         dup_y += dir[1]
+        #         new_pos = [dup_x, dup_y]
                 
-                horz_moves << new_pos # [[4,5],[5,5],[6,5],[7,5],[3,6]]
-            end #dup_x = 7 dup_y = 5
-            dup_x = dx.dup #3
-            dup_y = dy.dup #5
-        end
+        #         horz_moves << new_pos
+        #     end
+        #     in_front_x = new_pos[0] + dir[0]
+        #     in_front_y = new_pos[1] + dir[1]
+        #     final_pos = [in_front_x, in_front_y]
+        #     horz_moves << final_pos if self.board[final_pos].color != self.color #????????????
 
-        diagonal_dirs.each do |dir|
-            until invalid
-                dup_x += dir[0]
-                dup_y += dir[0]
-                new_pos = [dup_x, dup_y]
-                diag_moves << new_pos
-            end
-            dup_x = dx.dup
-            dup_y = dy.dup
-        end
+        #     dup_x = dx.dup
+        #     dup_y = dy.dup
+        #     new_pos = pos.dup
+        # end
+
+        # diagonal_dirs.each do |dir|
+        #     until !self.board.valid_pos?(new_pos) || self.board[new_pos] != self.null_piece
+        #         dup_x += dir[0]
+        #         dup_y += dir[1]
+        #         new_pos = [dup_x, dup_y]
+
+        #         diag_moves << new_pos
+        #     end
+        #     in_front_x = new_pos[0] + dir[0]
+        #     in_front_y = new_pos[1] + dir[1]
+        #     final_pos = [in_front_x, in_front_y]
+        #     diag_moves << final_pos if self.board[final_pos].color != self.color #????????????
+
+        #     dup_x = dx.dup
+        #     dup_y = dy.dup
+        #     new_pos = pos.dup
+        # end
     end
 
 end
--- Chess/pieces/rook.rb
+++ Chess/pieces/rook.rb
@@ -1,2 +1,8 @@
 class Rook
+    include Slideable
+
+    def move_dirs
+        horizontal_dirs
+    end
+
 end
